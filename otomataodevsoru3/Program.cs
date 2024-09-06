f594d5782576479e25e50b844ad119934884e2b5
Your Name <your.email@example.com>
2024-09-06 14:28:06 +0300

    Postfix_Prefix
    Otomata ödevim olan stringlerin özelleştirilerek postfix ve prefixini bulan program

---
+++ otomataodevsoru3/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int fix;
+        
+        Console.WriteLine("String giriniz");
+        string str = Console.ReadLine();
+        Console.WriteLine("Hedef stringi giriniz");
+        string hedef = Console.ReadLine();
+
+        if (hedef.Length > 6)
+        {
+            Console.WriteLine("5'ten küçük hedef giriniz ve tekrar deneyiniz...");
+        }
+        else if (str.Contains(hedef))// ana stringin içinde bulunuyor mu komutu
+        {
+            Console.WriteLine("Hedef string, ana stringin içinde bulunuyor.");
+
+            Console.WriteLine("Prefix için 1, Postfix için 2 tuşuna basınız");
+            fix = Convert.ToInt32(Console.ReadLine());
+
+            if (fix == 1)
+            {
+                Console.WriteLine("Prefix'ler:");
+
+                for (int i = 1; i <= hedef.Length; i++)
+                {
+                    string prefix = hedef.Substring(0, i);
+                    Console.WriteLine(prefix);
+                }
+            }
+            else if (fix == 2)
+            {
+                Console.WriteLine("Postfix'ler:");
+
+                for (int i = 1; i <= hedef.Length; i++)
+                {
+                    string postfix = hedef.Substring(hedef.Length - i, i);
+                    Console.WriteLine(postfix);
+                }
+            }
+
+        }
+        else
+        {
+            Console.WriteLine("Hedef string, ana stringin içinde bulunmuyor.");
+        }
+
+        Console.ReadKey();
+    }
+}
